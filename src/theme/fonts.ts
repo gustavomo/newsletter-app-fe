82fe5d4c94f58db36decee68c011737e6d167b43
gustavo <gustavo.moreno@cuemby.com>
2024-03-04 19:17:55 -0500

    removed unnecessary comments

--- src/theme/fonts.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  plusJakartaSansRegular: "PlusJakartaSans",
-  plusJakartaSansBoldFamily: "PlusJakartaSans Bold",
-  plusJakartaSansSemiBoldFamily: "PlusJakartaSans SemiBold",
-  plusJakartaSansExtraBoldFamily: "PlusJakartaSans ExtraBold",
-  plusJakartaSansLightFamily: "PlusJakartaSans Light",
-  plusJakartaSansExtraLightFamily: "PlusJakartaSans ExtraLight",
-  plusJakartaSansMedium: "PlusJakartaSans Medium",
-};
