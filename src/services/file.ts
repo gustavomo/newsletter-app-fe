cf13518903a89025d64dfa4975ce864e2b86a9b4
gustavo <gustavo.moreno@cuemby.com>
2024-03-04 19:34:21 -0500

    minor change

--- src/services/file.ts
+++ src/services/file.ts
@@ -3,7 +3,6 @@ import executeRequest from "./index";
 export const uploadFile: TUploadFile = async (params, file) => {
   try {
     const response = await executeRequest("/api/v1/upload", params, "POST", file);
-
     return response.url;
   } catch (error) {
     throw error;
